f200b66ca5dffbe541cc2c4b506cd591191e677b
Teng Sun <tengsun@cn.ibm.com>
2016-01-19 18:02:38 +0800

    update app with button event handling

--- app/src/main/java/st/geoquiz/QuizActivity.java
+++ app/src/main/java/st/geoquiz/QuizActivity.java
@@ -8,22 +8,45 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class QuizActivity extends AppCompatActivity {
+
+    private Button trueButton;
+    private Button falseButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // set init content view with layout
         setContentView(R.layout.activity_quiz);
+
+        // set toolbar
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        // set action listeners
+        setActionListeners();
+    }
+
+    private void setActionListeners() {
+        // assign value to buttons
+        trueButton = (Button) findViewById(R.id.button_true);
+        falseButton = (Button) findViewById(R.id.button_false);
+
+        // set event handlers
+        trueButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View v) {
+                Toast.makeText(QuizActivity.this, R.string.quiz_incorrect, Toast.LENGTH_SHORT).show();
+            }
+        });
+        falseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(QuizActivity.this, R.string.quiz_correct, Toast.LENGTH_SHORT).show();
             }
         });
     }
